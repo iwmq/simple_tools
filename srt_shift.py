6a50bbc3d5299282a7f9438b24a9a2747910ae62
Jay Wang <iwmq@qq.com>
2025-01-23 21:59:34 +0800

    fix types

--- srt_shift.py
+++ srt_shift.py
@@ -2,7 +2,7 @@
 """
 Simple script to shift time in srt subtitle file.
 """
-from datetime import timedelta
+
 import os
 import re
 import sys
@@ -10,7 +10,7 @@ from typing import Self
 
 
 # 00:01:15,550
-TIME_RE = re.compile(r'(-?\d{1,3}):(\d{2}):(\d{2}),(\d{3})')
+TIME_RE = re.compile(r"(-?\d{1,3}):(\d{2}):(\d{2}),(\d{3})")
 SEP = "-->"
 
 
@@ -26,9 +26,10 @@ class SubtitleTime:
     subtitle time format.
     """
 
-    __slots__ = ('hours', 'minutes', 'seconds', 'microseconds')
-    def __init__(self,
-        hours: int, minutes: int, seconds: int, microseconds: int
+    __slots__ = ("hours", "minutes", "seconds", "microseconds")
+
+    def __init__(
+        self, hours: int, minutes: int, seconds: int, microseconds: int
     ) -> None:
         self.hours = hours
         self.minutes = minutes
@@ -39,7 +40,7 @@ class SubtitleTime:
     def from_subtitle_time(cls, t: str) -> Self:
         match = TIME_RE.match(t.strip())
         if match is None:
-            raise SubtitleTimeParseError('No time delta found')
+            raise SubtitleTimeParseError("No time delta found")
         return cls.from_subtitile_match(match)
 
     @classmethod
@@ -48,18 +49,18 @@ class SubtitleTime:
 
         # Verify
         if hours < 0:
-            raise SubtitleTimeParseError('hours must be positive integer')
+            raise SubtitleTimeParseError("hours must be positive integer")
         if minutes < 0 or minutes >= 60:
-            raise SubtitleTimeParseError('minutes must be in [0, 60]')
+            raise SubtitleTimeParseError("minutes must be in [0, 60]")
         if seconds < 0 or seconds >= 60:
-            raise SubtitleTimeParseError('seconds must be in [0, 60]')
+            raise SubtitleTimeParseError("seconds must be in [0, 60]")
         if microseconds < 0:
-            raise SubtitleTimeParseError('microseconds must positive integer')
+            raise SubtitleTimeParseError("microseconds must positive integer")
 
         return cls(hours, minutes, seconds, microseconds)
 
     def to_subtitle_time(self):
-        return f'{self.hours:0>2}:{self.minutes:0>2}:{self.seconds:0>2},{self.microseconds:0>3}'
+        return f"{self.hours:0>2}:{self.minutes:0>2}:{self.seconds:0>2},{self.microseconds:0>3}"
 
     def __str__(self):
         return self.to_subtitle_time()
@@ -91,11 +92,11 @@ class SubtitleTime:
         else:
             self.minutes = minutes
             hour_detalta = 0
-        
+
         self.hours += hour_detalta
 
 
-def parse_time_span(line: str ) -> bool:
+def parse_time_span(line: str) -> tuple[SubtitleTime | None, SubtitleTime | None]:
     # 00:01:15,550 --> 00:01:17,790
     parts = line.partition(SEP)
     start = TIME_RE.match(parts[0].strip())
@@ -113,34 +114,35 @@ def parse_time_span(line: str ) -> bool:
 def shift_time_span(start: SubtitleTime, end: SubtitleTime, delta: float) -> str:
     start.add_seconds(delta)
     end.add_seconds(delta)
-    return f'{start} {SEP} {end}\n'
+    return f"{start} {SEP} {end}\n"
 
 
 def try_shift_line(line: str, delta: float) -> str | None:
     s, e = parse_time_span(line)
-    if None in (s, e):
+    if s and e:
+        new_line = shift_time_span(s, e, delta)
+        return new_line
+    else:
         return None
-    new_line = shift_time_span(s, e, delta)
-    return new_line
 
 
 def usage():
-    print('Usage: python3 srt_shift.py <srt_file> <seconds>')
+    print("Usage: python3 srt_shift.py <srt_file> <seconds>")
 
 
 def shift_srt(fpath: str, delta: float):
-    with open(fpath, encoding='utf-8') as f:
+    with open(fpath, encoding="utf-8") as f:
         new_lines = []
         for line in f.readlines():
             new_time_span_line = try_shift_line(line, delta)
             new_lines.append(new_time_span_line or line)
     base_path, ext = os.path.splitext(fpath)
-    new_fpath = f'{base_path}_shifted_{delta}{ext}'
-    with open(new_fpath, 'w', encoding='utf-8') as f:
+    new_fpath = f"{base_path}_shifted_{delta}{ext}"
+    with open(new_fpath, "w", encoding="utf-8") as f:
         f.writelines(new_lines)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 3:
         usage()
     else:
@@ -148,7 +150,7 @@ if __name__ == '__main__':
         delta_str = sys.argv[2]
 
         if not os.path.exists(fpath):
-            print('Specified srt file does not exist')
+            print("Specified srt file does not exist")
             sys.exit(127)
 
         try:
@@ -156,7 +158,8 @@ if __name__ == '__main__':
         except ValueError:
             delta = None
         if delta is None:
-            print('Specified shift delta is not numeric')
+            print("Specified shift delta is not numeric")
             sys.exit(127)
 
         shift_srt(fpath, delta)
+
